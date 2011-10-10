a6c8fecf86b84d58baba40b89dab9213721a3c9d
Geoff Hackett <ghackett@gmail.com>
2011-10-10 12:28:03 -0400

    rails projected created for ProviderOne

---
+++ ruby/ProviderOne/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProviderOne::Application.config.secret_token = '36705ae56348b96a62356a9adb61546a0d49ffc7778888ca3906d43186d0ae66f03603cf4a8563d4cf65f995c23fa0a02e83e761be4ab59ad9dd95a5c491b583'
