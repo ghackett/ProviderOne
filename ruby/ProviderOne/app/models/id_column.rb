cd36b9a517be893b5861086a081f8f6c29bc2534
Geoff Hackett <ghackett@gmail.com>
2011-11-04 16:22:51 -0400

    setting an objects id will now mark it as not new

--- ruby/ProviderOne/app/models/id_column.rb
+++ ruby/ProviderOne/app/models/id_column.rb
@@ -5,4 +5,8 @@ class IdColumn < LongColumn
     @camel_name = "id"
     @cap_camel_name = "Id"
   end
+
+    def get_base_table_methods()
+      return process_file_content(File.read("public/templates/columns/id_column/BaseTableMethods.java"))
+    end
 end
---
+++ ruby/ProviderOne/public/templates/columns/id_column/BaseTableMethods.java
@@ -0,0 +1,10 @@
+    public {JavaType} get{CapCamelName}() {
+        return m{CapCamelName};
+    }
+
+    public void set{CapCamelName}({JavaType} {CamelName}) {
+        m{CapCamelName} = {CamelName};
+        m{CapCamelName}Set = true;
+        mIsNew = {CamelName} != null;
+    }
+
